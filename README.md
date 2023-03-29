b6a22c2ec6d2951113a33322cfabe750d0511c5f
Bryan C Guner <bryan.guner@gmail.com>
2023-03-29 17:14:18 -0400

    echoing a variable in php

--- README.md
+++ README.md
@@ -28,3 +28,18 @@
 ?>
 
 ```
+
+
+**Echoing a variable in php:**
+
+```php
+
+<?php 
+echo 2 + 2;
+ $myname = "Bryan";
+?>
+<h1>This page is about <?php echo $myname ?><h1>
+    <?php echo 5* 5 ?>
+<h2>All about <?php echo $myname ?><h2>
+
+```
--- app/public/experiment.php
+++ app/public/experiment.php
@@ -1,12 +1,7 @@
 <?php 
 echo 2 + 2;
  $myname = "Bryan";
-
-
-
-
 ?>
-
-<h1>This page is about Bryan<h1>
+<h1>This page is about <?php echo $myname ?><h1>
     <?php echo 5* 5 ?>
-<h2>All about Bryan<h2>
+<h2>All about <?php echo $myname ?><h2>
