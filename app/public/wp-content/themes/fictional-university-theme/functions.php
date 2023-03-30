5dbde9b0d9a3a17e310f1ae2c0e863707caeb061
Bryan C Guner <bryan.guner@gmail.com>
2023-03-30 10:07:17 -0400

    added action to functions.php to set up title tag for each page

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -7,3 +7,8 @@ function university_files(){
     wp_enqueue_script('main-university-javascript', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
 }
 add_action('wp_enqueue_scripts', 'university_files');
+
+function university_features(){
+    add_theme_support('title-tag');
+}
+add_action('after_setup_theme', 'university_features');
